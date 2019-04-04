695aca13d180c19111adabc39d1d6c72d0cbfd11
Unknown <christopher.ryan.miller@gmail.com>
2019-04-03 20:40:03 -0600

    Notes on model issues
    Gaussian processes may not be the most suitable, given some of the specifics of this problem

---
+++ PGM/08_model_improvements.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 18:30:38 2019
+
+@author: Chris
+"""
+
+# Notes:
+# De-weight value of observations for dry period? They're getting equal weight, which seems off
+# Alternatively, downsample less?
+
+
+# Also: Check how a fitted model does on test data. 
+
+# Also also: Correlation should maybe be stronger? Weaker?  Different, definitely. Maybe too strong of correlation is 
+
+
